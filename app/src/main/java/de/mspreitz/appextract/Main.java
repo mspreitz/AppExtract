6802e35bef194b93c2250cc973dcf4fc223b0184
mspreitz <research@spreitzenbarth.de>
2015-03-05 15:33:20 +0100

    now generating md5 hashes of all installed apps

--- app/src/main/java/de/mspreitz/appextract/Main.java
+++ app/src/main/java/de/mspreitz/appextract/Main.java
@@ -10,17 +10,26 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import com.stericson.RootTools.RootTools;
 import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.math.BigInteger;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.io.InputStream;
+
 
 public class Main extends Activity {
 
@@ -81,40 +90,105 @@ public class Main extends Activity {
         sendMessage();
     }
 
+    public static String calculateMD5(String apkFile) {
+        MessageDigest digest;
+        String output = "unknown";
+        try{
+            digest = MessageDigest.getInstance("MD5");
+            try {
+                InputStream is = new FileInputStream(apkFile);
+                byte[] buffer = new byte[8192];
+                int read;
+                try {
+                    while ((read = is.read(buffer)) > 0) {
+                        digest.update(buffer, 0, read);
+                    }
+                    byte[] md5sum = digest.digest();
+                    BigInteger bigInt = new BigInteger(1, md5sum);
+                    output = bigInt.toString(16);
+                    output = String.format("%32s", output).replace(' ', '0');
+                    is.close();
+                } catch (IOException e) {
+                    throw new RuntimeException("Unable to calculate MD5", e);
+                }
+            }catch (FileNotFoundException e){
+                Log.e("MD5", "apk file not found", e);
+            }
+        }catch (NoSuchAlgorithmException e){
+            Log.e("MD5", "No md5 algorithm available", e);
+        }
+        return output;
+    }
+
     public void sendMessage() {
         final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
         mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
         PackageManager pm = getPackageManager();
         List<ApplicationInfo> apps = pm.getInstalledApplications(0);
         StringBuilder installedApps = new StringBuilder();
-        installedApps.append("Type;App_Name;TargetSdkVersion;Package_Name;Process_Name;Version_Code;Version_Name;Certificate_Info;Certificate_SN;InstallTime;LastModified\n");
+        installedApps.append("Type;App_Name;md5;TargetSdkVersion;Package_Name;Process_Name;APK_Location;Version_Code;Version_Name;Certificate_Info;Certificate_SN;InstallTime;LastModified\n");
         for(ApplicationInfo app : apps) {
             if ((app.flags & ApplicationInfo.FLAG_SYSTEM) == 1) {
                 try{
-                    installedApps.append("SystemApp;").append(pm.getApplicationLabel(app)).append(";").append(app.targetSdkVersion).append(";").append(app.packageName).append(";")
-                            .append(app.processName).append(";").append(pm.getPackageInfo(app.packageName, 0).versionCode).append(";").append(pm.getPackageInfo(app.packageName, 0).versionName).append(";")
+                    String md5 = calculateMD5(app.sourceDir);
+                    installedApps.append("SystemApp;")
+                            .append(pm.getApplicationLabel(app)).append(";")
+                            .append(md5).append(";")
+                            .append(app.targetSdkVersion).append(";")
+                            .append(app.packageName).append(";")
+                            .append(app.processName).append(";")
+                            .append(app.sourceDir).append(";")
+                            .append(pm.getPackageInfo(app.packageName, 0).versionCode).append(";")
+                            .append(pm.getPackageInfo(app.packageName, 0).versionName).append(";")
                             .append(((X509Certificate) CertificateFactory.getInstance("X509").generateCertificate(new ByteArrayInputStream(pm.getPackageInfo(app.packageName, PackageManager.GET_SIGNATURES).signatures[0].toByteArray()))).getSubjectDN()).append(";")
                             .append(((X509Certificate) CertificateFactory.getInstance("X509").generateCertificate(new ByteArrayInputStream(pm.getPackageInfo(app.packageName, PackageManager.GET_SIGNATURES).signatures[0].toByteArray()))).getSerialNumber()).append(";")
-                            //.append(new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(new Date((pm.getPackageInfo(app.packageName, 0).firstInstallTime))))
-                            .append(";")
-                                    //.append(new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(new Date((pm.getPackageInfo(app.packageName, 0).lastUpdateTime))))
-                            .append("\n");
+                            .append("unknown").append(";")
+                            .append("unknown").append("\n");
                 }catch(Exception e){
-                    installedApps.append("SystemApp;").append(pm.getApplicationLabel(app)).append(";").append(app.targetSdkVersion).append(";").append(app.packageName).append(";")
-                            .append(app.processName).append(";").append(";").append(";").append(";").append(";").append("\n");
+                    installedApps.append("SystemApp;")
+                            .append(pm.getApplicationLabel(app)).append(";")
+                            .append("unknown").append(";")
+                            .append(app.targetSdkVersion).append(";")
+                            .append(app.packageName).append(";")
+                            .append(app.processName).append(";")
+                            .append("unknown").append(";")
+                            .append("unknown").append(";")
+                            .append("unknown").append(";")
+                            .append("unknown").append(";")
+                            .append("unknown").append(";")
+                            .append("unknown").append(";")
+                            .append("unknown").append("\n");
                 }
             } else {
                 try{
-                    installedApps.append("UserApp;").append(pm.getApplicationLabel(app)).append(";").append(app.targetSdkVersion).append(";").append(app.packageName).append(";")
-                            .append(app.processName).append(";").append(pm.getPackageInfo(app.packageName, 0).versionCode).append(";").append(pm.getPackageInfo(app.packageName, 0).versionName).append(";")
+                    String md5 = calculateMD5(app.sourceDir);
+                    installedApps.append("UserApp;")
+                            .append(pm.getApplicationLabel(app)).append(";")
+                            .append(md5).append(";")
+                            .append(app.targetSdkVersion).append(";")
+                            .append(app.packageName).append(";")
+                            .append(app.processName).append(";")
+                            .append(app.sourceDir).append(";")
+                            .append(pm.getPackageInfo(app.packageName, 0).versionCode).append(";")
+                            .append(pm.getPackageInfo(app.packageName, 0).versionName).append(";")
                             .append(((X509Certificate) CertificateFactory.getInstance("X509").generateCertificate(new ByteArrayInputStream(pm.getPackageInfo(app.packageName, PackageManager.GET_SIGNATURES).signatures[0].toByteArray()))).getSubjectDN()).append(";")
                             .append(((X509Certificate) CertificateFactory.getInstance("X509").generateCertificate(new ByteArrayInputStream(pm.getPackageInfo(app.packageName, PackageManager.GET_SIGNATURES).signatures[0].toByteArray()))).getSerialNumber()).append(";")
                             .append(new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(new Date((pm.getPackageInfo(app.packageName, 0).firstInstallTime)))).append(";")
-                            .append(new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(new Date((pm.getPackageInfo(app.packageName, 0).lastUpdateTime))))
-                            .append("\n");
+                            .append(new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(new Date((pm.getPackageInfo(app.packageName, 0).lastUpdateTime)))).append("\n");
                 }catch(Exception e){
-                    installedApps.append("UserApp;").append(pm.getApplicationLabel(app)).append(";").append(app.targetSdkVersion).append(";").append(app.packageName).append(";")
-                            .append(app.processName).append(";").append(";").append(";").append(";").append(";").append("\n");
+                    installedApps.append("UserApp;")
+                            .append(pm.getApplicationLabel(app)).append(";")
+                            .append("unknown").append(";")
+                            .append(app.targetSdkVersion).append(";")
+                            .append(app.packageName).append(";")
+                            .append(app.processName).append(";")
+                            .append("unknown").append(";")
+                            .append("unknown").append(";")
+                            .append("unknown").append(";")
+                            .append("unknown").append(";")
+                            .append("unknown").append(";")
+                            .append("unknown").append(";")
+                            .append("unknown").append("\n");
                 }
             }
         }
